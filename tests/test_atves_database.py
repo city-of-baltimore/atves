d997bbf0e453a3f999e42f0daa45383f2cf28654
Brian Seel <Brian.Seel@baltimorecity.gov>
2021-03-11 13:01:59 -0500

    Updating tests

--- tests/test_atves_database.py
+++ tests/test_atves_database.py
@@ -14,10 +14,6 @@ def test_atvesdb_build_db_conduent_red_light(atvesdb_fixture, conn_str):
     engine = create_engine(conn_str, echo=True, future=True)
     with Session(bind=engine, future=True) as session:
         ret = session.query(AtvesCamLocations.cam_type).filter(AtvesCamLocations.cam_type == 'RL')
-        assert ret.count() == 0
-
-        atvesdb_fixture._build_db_conduent_red_light()
-        ret = session.query(AtvesCamLocations.cam_type).filter(AtvesCamLocations.cam_type == 'RL')
         assert ret.count() > 100
 
 
@@ -26,10 +22,6 @@ def test_atvesdb_build_db_conduent_overheight(atvesdb_fixture, conn_str):
     engine = create_engine(conn_str, echo=True, future=True)
     with Session(bind=engine, future=True) as session:
         ret = session.query(AtvesCamLocations.cam_type).filter(AtvesCamLocations.cam_type == 'OH')
-        assert ret.count() == 0
-
-        atvesdb_fixture._build_db_conduent_overheight()
-        ret = session.query(AtvesCamLocations.cam_type).filter(AtvesCamLocations.cam_type == 'OH')
         assert ret.count() > 10
 
 
@@ -37,10 +29,6 @@ def test_atvesdb_build_db_speed_cameras(atvesdb_fixture, conn_str):
     """Testing _build_db_speed_cameras"""
     engine = create_engine(conn_str, echo=True, future=True)
     with Session(bind=engine, future=True) as session:
-        ret = session.query(AtvesCamLocations.cam_type).filter(AtvesCamLocations.cam_type == 'SC')
-        assert ret.count() == 0
-
-        atvesdb_fixture._build_db_speed_cameras()
         ret = session.query(AtvesCamLocations.cam_type).filter(AtvesCamLocations.cam_type == 'SC')
         assert ret.count() > 10
 
